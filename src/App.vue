e0bb7c64b65b81bbbe4e25b070174934a1ce46a0
Justin Navarro <info@justinnavarro.net>
2020-08-27 14:50:37 -0400

    more styling configuration

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,11 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-export default defineComponent({});
+export default defineComponent({
+	setup() {
+		console.log('test');
+	},
+});
 </script>
 
 <style>
