c86c5179f6b7be8e55f7c92d3e747d7de5aabbcf
Justin Navarro <info@justinnavarro.net>
2020-08-27 22:10:41 -0400

    start setting up Input component

--- src/components/ui-elements/form/Input.vue
+++ src/components/ui-elements/form/Input.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, computed } from 'vue';
 
 export default defineComponent({
 	props: {
@@ -7,20 +7,45 @@ export default defineComponent({
 			required: true,
 			type: String,
 		},
+		rules: {
+			default: () => [],
+			type: Array,
+		},
 		type: {
 			default: 'text',
 			type: String,
 		},
+		value: {
+			required: true,
+			type: String,
+		},
+	},
+	setup(props, { emit }) {
+		const labelId = computed(() =>
+			props.label.toLowerCase().replace(/\s+/, '-')
+		);
+
+		const updateValue = (e: string) => {
+			emit('update:value', e);
+		};
+
+		return {
+			labelId,
+			updateValue,
+		};
 	},
 });
 </script>
 
 <template>
 	<div class="mb-4">
-		<label for="" class="text-sm text-gray-600">{{ label }}</label>
+		<label :for="labelId" class="text-sm text-gray-600">{{ label }}</label>
 		<input
+			:id="labelId"
 			class="w-full p-2 mt-2 border border-gray-300 rounded focus:border-primary outline-none"
 			:type="type"
+			:value="value"
+			@input="updateValue($event.target.value)"
 		/>
 	</div>
 </template>
