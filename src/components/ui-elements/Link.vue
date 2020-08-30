d3d275ba04e30cde1d60de2d8062bf3bdda49edc
Justin Navarro <info@justinnavarro.net>
2020-08-30 00:32:30 -0400

    card and link components

---
+++ src/components/ui-elements/Link.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	props: {
+		linkTo: {
+			required: true,
+		},
+	},
+});
+</script>
+
+<template>
+	<router-link
+		:to="linkTo"
+		class="mr-6 text-gray-600 hover:text-primary active:text-primary flex flex-row items-center transition ease-out duration-300 py-4"
+		active-class="text-primary"
+		exact
+	>
+		<slot></slot>
+	</router-link>
+</template>
