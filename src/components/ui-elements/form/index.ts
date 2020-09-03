5749a4c8e6c8ae8a5df2e0a96d8f9940bb62d488
Justin Navarro <info@justinnavarro.net>
2020-09-03 10:32:24 -0400

    created label component

--- src/components/ui-elements/form/index.ts
+++ src/components/ui-elements/form/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from './Button.vue';
 export { default as Form } from './Form';
 export { default as Input } from './Input.vue';
+export { default as Label } from './Label.vue';
 export { default as Select } from './Select.vue';
