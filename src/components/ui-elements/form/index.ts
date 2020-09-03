b832a264adb85160da0fa9a4fea5259e297bf96a
Justin Navarro <info@justinnavarro.net>
2020-09-02 22:51:53 -0400

    form export

---
+++ src/components/ui-elements/form/index.ts
@@ -0,0 +1,4 @@
+export { default as Button } from './Button.vue';
+export { default as Form } from './Form';
+export { default as Input } from './Input.vue';
+export { default as Select } from './Select.vue';
--- src/components/ui-elements/index.ts
+++ src/components/ui-elements/index.ts
@@ -7,9 +7,5 @@ export { default as CardContent } from './card/CardContent.vue';
 export { default as CardFooter } from './card/CardFooter.vue';
 export { default as CardHeader } from './card/CardHeader.vue';
 
-export { default as Button } from './form/Button.vue';
-export { default as Form } from './form/Form';
-export { default as Input } from './form/Input.vue';
-export { default as Select } from './form/Select.vue';
-
+export * from './form';
 export * from './icons';
