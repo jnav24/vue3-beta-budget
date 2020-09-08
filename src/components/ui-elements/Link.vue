7673f35c10b908025cbbe091e7b5c766e14b6cdd
Justin Navarro <info@justinnavarro.net>
2020-09-07 22:12:36 -0400

    prevent mobile nav from dropping content down

--- src/components/ui-elements/Link.vue
+++ src/components/ui-elements/Link.vue
@@ -20,9 +20,14 @@ export default defineComponent({
 		:class="{
 			'mr-6 text-gray-600 hover:text-primary active:text-primary flex flex-row items-center transition ease-out duration-300 py-4':
 				linkType === 'default',
-			'text-white hover:bg-white hover:bg-opacity-25 flex flex-row items-center p-2 rounded transition ease-out-in duration-300 mb-1': linkType === 'inverted',
+			'text-white hover:bg-white hover:bg-opacity-25 flex flex-row items-center p-2 rounded transition ease-out-in duration-300 mb-1':
+				linkType === 'inverted',
 		}"
-		:active-class="linkType === 'default' ? `text-primary` : `text-white bg-dark-primary hover:bg-dark-primary hover:bg-opacity-100`"
+		:active-class="
+			linkType === 'default'
+				? `text-primary`
+				: `text-white bg-dark-primary hover:bg-dark-primary hover:bg-opacity-100`
+		"
 		exact
 	>
 		<slot></slot>
