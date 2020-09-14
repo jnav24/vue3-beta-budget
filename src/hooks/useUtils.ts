d9413c7180222282905423fb4b52840818688efc
Justin Navarro <info@justinnavarro.net>
2020-09-14 00:05:00 -0400

    add arrayColumn

--- src/hooks/useUtils.ts
+++ src/hooks/useUtils.ts
@@ -2,5 +2,14 @@ export default function useUtils() {
 	const ucFirst = (val: string): string =>
 		val.charAt(0).toUpperCase() + val.slice(1);
 
-	return { ucFirst };
+	const arrayColumn = (
+		val: string,
+		arr: Array<Record<string, any>>
+	): string[] => {
+		return arr
+			.map((obj: any) => obj[val] ?? null)
+			.filter((value: any) => value);
+	};
+
+	return { arrayColumn, ucFirst };
 }
