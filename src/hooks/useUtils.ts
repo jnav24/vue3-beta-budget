9eef22ac0be8e6985078440a9255f00b232177b1
Justin Navarro <info@justinnavarro.net>
2020-09-14 12:04:13 -0400

    fixed types in arrayColumn

--- src/hooks/useUtils.ts
+++ src/hooks/useUtils.ts
@@ -2,13 +2,13 @@ export default function useUtils() {
 	const ucFirst = (val: string): string =>
 		val.charAt(0).toUpperCase() + val.slice(1);
 
-	const arrayColumn = (
-		val: string,
-		arr: Array<Record<string, any>>
-	): string[] => {
+	const arrayColumn = <S extends keyof R, R extends Record<string, string>>(
+		val: S,
+		arr: Array<R>
+	): Array<R[S]> => {
 		return arr
-			.map((obj: any) => obj[val] ?? null)
-			.filter((value: any) => value);
+			.map((obj: R) => obj[val] ?? null)
+			.filter((value: R[S]) => value);
 	};
 
 	return { arrayColumn, ucFirst };
