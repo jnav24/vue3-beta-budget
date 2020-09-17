96798beb5bde1e1b806b0ea384aa6afa1e0d509c
Justin Navarro <info@justinnavarro.net>
2020-09-17 17:58:53 -0400

    initial setup of budget edit page

---
+++ src/components/partials/BudgetEditSummary.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="bg-dark-primary w-full">
+		<div class="container mx-auto py-8">
+			<p>Total Earned</p>
+			<p>Total Saved</p>
+			<p>Total Spent</p>
+			<p>Total in Banks</p>
+			<p>Total in Invested</p>
+		</div>
+	</div>
+</template>
--- src/views/dashboard/BudgetEdit.vue
+++ src/views/dashboard/BudgetEdit.vue
@@ -1,9 +1,17 @@
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
+import BudgetEditSummary from '@/components/partials/BudgetEditSummary.vue';
+import Card from '@/components/ui-elements/card/Card.vue';
+import CardContent from '@/components/ui-elements/card/CardContent.vue';
+import CardHeader from '@/components/ui-elements/card/CardHeader.vue';
 import SideBar from '@/components/partials/SideBar.vue';
 
 export default defineComponent({
 	components: {
+		BudgetEditSummary,
+		Card,
+		CardContent,
+		CardHeader,
 		SideBar,
 	},
 	setup() {
@@ -20,11 +28,7 @@ export default defineComponent({
 </script>
 
 <template>
-	<div class="bg-dark-primary w-full">
-		<div class="container mx-auto py-8">
-			<p>Hello</p>
-		</div>
-	</div>
+	<BudgetEditSummary />
 
 	<div class="container mx-auto py-6">
 		<div class="grid grid-cols-4 gap-3">
@@ -33,6 +37,16 @@ export default defineComponent({
 				:items="categories"
 				v-model:selected-item="selectedCategory"
 			/>
+
+			<Card class="col-span-3">
+				<CardHeader class="bg-gray-100">
+					Header
+				</CardHeader>
+
+				<CardContent>
+					content...
+				</CardContent>
+			</Card>
 		</div>
 	</div>
 </template>
