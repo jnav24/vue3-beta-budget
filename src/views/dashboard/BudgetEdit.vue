1c6b73d91615670540cc0a72f844f4ad055ca896
Justin Navarro <info@justinnavarro.net>
2020-09-18 22:37:04 -0400

    budget edit headers

--- src/views/dashboard/BudgetEdit.vue
+++ src/views/dashboard/BudgetEdit.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { computed, defineComponent, ref } from 'vue';
 import BudgetEditSummary from '@/components/partials/BudgetEditSummary.vue';
 import Card from '@/components/ui-elements/card/Card.vue';
 import CardContent from '@/components/ui-elements/card/CardContent.vue';
@@ -21,8 +21,66 @@ export default defineComponent({
 			{ label: 'Credit Cards', value: 'credit-cards' },
 			{ label: 'Investments', value: 'investments' },
 		];
+		const headers: Record<string, Array<string>> = {
+			common: [
+				'',
+				'name',
+				'type',
+				'amount',
+				'due date/paid date',
+				'actions',
+			],
+			'credit-cards': [
+				'',
+				'name',
+				'type',
+				'amount',
+				'balance',
+				'limit',
+				'due date/paid date',
+				'actions',
+			],
+			income: ['name', 'type', 'amount', 'paid date', 'actions'],
+			miscellaneous: [
+				'',
+				'name',
+				'amount',
+				'due date/paid date',
+				'actions',
+			],
+			savings: ['name', 'type', 'amount', 'actions'],
+			vehicles: [
+				'',
+				'name',
+				'type',
+				'amount',
+				'balance',
+				'due date/paid date',
+				'actions',
+			],
+		};
 
-		return { categories, selectedCategory };
+		const categoryHeader = computed(() => {
+			const categoryMap: Record<string, string> = {
+				banks: 'savings',
+				investments: 'savings',
+			};
+
+			if (headers[selectedCategory.value]) {
+				return headers[selectedCategory.value];
+			}
+
+			if (
+				!headers[selectedCategory.value] &&
+				categoryMap[selectedCategory.value]
+			) {
+				return headers[categoryMap[selectedCategory.value]];
+			}
+
+			return headers.common;
+		});
+
+		return { categories, categoryHeader, selectedCategory };
 	},
 });
 </script>
@@ -40,7 +98,16 @@ export default defineComponent({
 
 			<Card class="col-span-3">
 				<CardHeader class="bg-gray-100">
-					Header
+					<div
+						:class="`grid gap-2 grid-cols-${categoryHeader.length}`"
+					>
+						<div
+							v-for="(header, index) in categoryHeader"
+							:key="index"
+						>
+							{{ header }}
+						</div>
+					</div>
 				</CardHeader>
 
 				<CardContent>
