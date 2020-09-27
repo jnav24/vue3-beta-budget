0d84543930d3ab55bf555b2da5c62280bb386fbe
Justin Navarro <info@justinnavarro.net>
2020-09-26 23:34:32 -0400

    reporting form component

---
+++ src/components/partials/ReportsForm.vue
@@ -0,0 +1,107 @@
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import Button from '@/components/ui-elements/form/Button.vue';
+import Card from '@/components/ui-elements/card/Card.vue';
+import Form from '@/components/ui-elements/form/Form';
+import Label from '@/components/ui-elements/form/Label.vue';
+import Select from '@/components/ui-elements/form/Select.vue';
+import useTimestamp from '@/hooks/useTimestamp';
+import { useTypesStore } from '@/store';
+
+export default defineComponent({
+	components: {
+		Button,
+		Card,
+		Form,
+		Label,
+		Select,
+	},
+	setup() {
+		const { getAllMonths } = useTimestamp();
+		const typesStore = useTypesStore();
+		const form = {
+			end_month: {
+				rules: [],
+				value: '',
+			},
+			start_month: {
+				rules: [],
+				value: '',
+			},
+			type: {
+				rules: [],
+				value: '',
+			},
+			year: {
+				rules: [],
+				value: '',
+			},
+		};
+		const isFormValid = ref(false);
+		const types: any[] = typesStore.bills;
+		const years: any[] = [{ value: '2020', label: '2020' }];
+
+		return {
+			form,
+			isFormValid,
+			months: getAllMonths('abbr'),
+			types,
+			years,
+		};
+	},
+});
+</script>
+
+<template>
+	<Card>
+		<Form v-model:valid="isFormValid">
+			<div class="mb-6">
+				<Label label="Expense Type" />
+				<Select
+					class="z-40"
+					:rules="form.type.rules"
+					:items="types"
+					item-value="slug"
+					item-label="name"
+					label="Expense Type"
+					v-model:value="form.type.value"
+				/>
+			</div>
+
+			<div class="mb-6">
+				<Label label="Year" />
+				<Select
+					class="z-30"
+					:rules="form.year.rules"
+					:items="years"
+					label="Year"
+					v-model:value="form.year.value"
+				/>
+			</div>
+
+			<div class="mb-6">
+				<Label label="Start Month" />
+				<Select
+					class="z-20"
+					:rules="form.start_month.rules"
+					:items="months"
+					label="Start Month"
+					v-model:value="form.start_month.value"
+				/>
+			</div>
+
+			<div class="mb-6">
+				<Label label="End Month" />
+				<Select
+					class="z-10"
+					:rules="form.end_month.rules"
+					:items="months"
+					label="End Month"
+					v-model:value="form.end_month.value"
+				/>
+			</div>
+
+			<Button class="w-full" :is-disabled="!isFormValid">Search</Button>
+		</Form>
+	</Card>
+</template>
--- src/views/dashboard/Report.vue
+++ src/views/dashboard/Report.vue
@@ -1,96 +1,22 @@
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
-import Form from '@/components/ui-elements/form/Form';
-import Label from '@/components/ui-elements/form/Label.vue';
-import Select from '@/components/ui-elements/form/Select.vue';
-import { useTypesStore } from '@/store/types';
+import { defineComponent } from 'vue';
+import ReportsForm from '@/components/partials/ReportsForm.vue';
 
 export default defineComponent({
 	components: {
-		Form,
-		Label,
-		Select,
+		ReportsForm,
 	},
 	setup() {
-		const typesStore = useTypesStore();
-		const form = {
-			end_month: {
-				rules: [],
-				value: '',
-			},
-			start_month: {
-				rules: [],
-				value: '',
-			},
-			type: {
-				rules: [],
-				value: '',
-			},
-			year: {
-				rules: [],
-				value: '',
-			},
-		};
-		const isFormValid = ref(false);
-		const types: any[] = typesStore.bills;
-		const years: any[] = [];
-		return { form, isFormValid, types, years };
+		return {};
 	},
 });
 </script>
 
 <template>
 	<main class="container mx-auto py-6">
-		<p>Report Type Year Start/end months</p>
 		<div class="grid grid-cols-6 gap-2">
 			<aside class="col-span-1">
-				<Form v-model:valid="isFormValid">
-					<div class="mb-4">
-						<Label label="Expense Type" />
-						<Select
-							class="z-40"
-							:rules="form.type.rules"
-							:items="types"
-							item-value="slug"
-							item-label="name"
-							label="Expense Type"
-							:value="form.type.value"
-						/>
-					</div>
-
-					<div class="mb-4">
-						<Label label="Year" />
-						<Select
-							class="z-30"
-							:rules="form.year.rules"
-							:items="types"
-							label="Expense Type"
-							:value="form.year.value"
-						/>
-					</div>
-
-					<div class="mb-4">
-						<Label label="Start Month" />
-						<Select
-							class="z-20"
-							:rules="form.year.rules"
-							:items="types"
-							label="Expense Type"
-							:value="form.year.value"
-						/>
-					</div>
-
-					<div class="mb-4">
-						<Label label="End Month" />
-						<Select
-							class="z-10"
-							:rules="form.year.rules"
-							:items="types"
-							label="Expense Type"
-							:value="form.year.value"
-						/>
-					</div>
-				</Form>
+				<ReportsForm />
 			</aside>
 		</div>
 	</main>
