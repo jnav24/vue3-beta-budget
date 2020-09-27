af0e90cd07090b0a9f73482b3097b80b060fd077
Justin Navarro <info@justinnavarro.net>
2020-09-27 12:32:09 -0400

    fixed validation rules

--- src/hooks/useFormValidation.ts
+++ src/hooks/useFormValidation.ts
@@ -1,49 +1,5 @@
 import useUtils from './useUtils';
 
-export default function useFormValidation() {
-	const validateInput = (type: string, value: string): string | boolean => {
-		const func: any = `validate${useUtils().ucFirst(type)}`;
-
-		if (typeof eval(func) === 'function') {
-			return eval(func)(value);
-		}
-
-		return '';
-	};
-
-	const validateRules = (
-		value: string,
-		rules: Record<string, string>
-	): { error: null | string; valid: boolean } => {
-		let tempValid = true;
-		let error = null;
-
-		for (const [type, message] of Object.entries(rules)) {
-			const isValid = validateInput(type, value);
-
-			if (!tempValid) {
-				continue;
-			}
-
-			if ((!isValid || typeof isValid !== 'boolean') && tempValid) {
-				error = message;
-				tempValid = false;
-				continue;
-			}
-
-			error = null;
-			tempValid = true;
-		}
-
-		return {
-			error,
-			valid: tempValid,
-		};
-	};
-
-	return { validateInput, validateRules };
-}
-
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function validateEmail(email: string): boolean {
 	return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
@@ -71,5 +27,58 @@ function validateSame(value: string, confirm: string): boolean {
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function validateNumeric(value: string): boolean {
-	return /\d/.test(value);
+	return /^\d+$/.test(value);
+}
+
+export default function useFormValidation() {
+	const defaultErrorMessages: Record<string, string> = {
+		required: 'Field is required',
+	};
+
+	const validateInput = (type: string, value: string): boolean => {
+		const func: any = `validate${useUtils().ucFirst(type)}`;
+
+		try {
+			return eval(func)(value);
+		} catch (err) {
+			throw `Function for type '${type}', does not exist`;
+		}
+	};
+
+	const validateRules = (
+		inputValue: string,
+		rules: Record<string, string>
+	): { error: null | string; valid: boolean } => {
+		let tempValid = true;
+		let error = null;
+
+		for (const [key, value] of Object.entries(rules)) {
+			const isNumeric = validateNumeric(key);
+			const type = isNumeric ? value : key;
+			const message = isNumeric
+				? defaultErrorMessages[value] ?? defaultErrorMessages.required
+				: value;
+			const isValid = validateInput(type, inputValue);
+
+			if (!tempValid) {
+				continue;
+			}
+
+			if (!isValid && tempValid) {
+				error = message;
+				tempValid = false;
+				continue;
+			}
+
+			error = null;
+			tempValid = true;
+		}
+
+		return {
+			error,
+			valid: tempValid,
+		};
+	};
+
+	return { validateInput, validateRules };
 }
