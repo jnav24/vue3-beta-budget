7ed922771001f28f90d76c4b465a0d13c1e37587
Justin Navarro <info@justinnavarro.net>
2020-10-03 13:41:50 -0400

    show arrow up/down based on percentage

--- src/components/partials/ReportsSummary.vue
+++ src/components/partials/ReportsSummary.vue
@@ -1,10 +1,12 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
+import ArrowDownIcon from '@/components/ui-elements/icons/ArrowDownIcon.vue';
 import ArrowUpIcon from '@/components/ui-elements/icons/ArrowUpIcon.vue';
 import Card from '@/components/ui-elements/card/Card.vue';
 
 export default defineComponent({
 	components: {
+		ArrowDownIcon,
 		ArrowUpIcon,
 		Card,
 	},
@@ -38,8 +40,22 @@ export default defineComponent({
 				class="text-sm text-gray-500 flex flex-row items-baseline ml-2 space-x-1"
 			>
 				<template v-if="percentage">
-					<ArrowUpIcon class="w-4 h-4 text-primary" />
-					<span class="text-primary">{{ percentage }}%</span>
+					<ArrowDownIcon
+						class="w-4 h-4 text-danger"
+						v-if="percentage < 0"
+					/>
+					<ArrowUpIcon
+						class="w-4 h-4 text-primary"
+						v-if="percentage >= 0"
+					/>
+					<span
+						:class="{
+							'text-primary': percentage >= 0,
+							'text-danger': percentage < 0,
+						}"
+					>
+						{{ percentage.replace('-', '') }}%
+					</span>
 				</template>
 
 				<template v-if="text">{{ text }}</template>
