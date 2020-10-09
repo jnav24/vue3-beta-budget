48ee9fdd5b453897698e5bee3c6412820f4aa5f6
Justin Navarro <info@justinnavarro.net>
2020-10-09 00:41:31 -0400

    start connecting the login page to the backend

---
+++ src/hooks/useHttp.ts
@@ -0,0 +1,126 @@
+import { reactive, toRefs } from 'vue';
+import axios, { AxiosResponse } from 'axios';
+
+enum URLMethods {
+	GET = 'get',
+	POST = 'post',
+	PUT = 'put',
+	DELETE = 'delete',
+}
+
+type URLInterface = {
+	method?: URLMethods;
+	path: string;
+	params?: Record<string, any>;
+	headers?: Record<string, any>;
+};
+
+export type HttpResponse = {
+	loading: boolean;
+	data: any;
+	error: string;
+	success: boolean;
+};
+
+export default function useHttp() {
+	const state: HttpResponse = reactive({
+		loading: false,
+		data: {},
+		error: '',
+		success: true,
+	});
+
+	const getResponse = async ({
+		method,
+		path,
+		params,
+		headers,
+	}: URLInterface) => {
+		try {
+			state.loading = true;
+			let responseData: any = {
+				data: params,
+			};
+
+			if (method === 'get') {
+				responseData = { params };
+			}
+
+			const response: AxiosResponse = await axios({
+				method,
+				url: `${process.env.VUE_APP_API_DOMAIN}/${path}`,
+				headers,
+				...responseData,
+			});
+
+			if (response.status >= 200 && response.status < 300) {
+				state.success = true;
+				state.data = response.data;
+			} else {
+				state.success = false;
+			}
+		} catch (error) {
+			let msg =
+				'Something unexpected has occurred. Please try again later.';
+
+			if (error.response.data.message) {
+				msg = error.response.data.message;
+			}
+
+			state.success = false;
+			state.error = msg;
+		} finally {
+			state.loading = false;
+		}
+
+		return state;
+	};
+
+	const get = async ({ path, params }: URLInterface) => {
+		return getResponse({
+			method: URLMethods.GET,
+			path,
+			params: params || {},
+			headers: {},
+		});
+	};
+
+	const getAuth = async ({ path, params }: URLInterface) => {
+		return getResponse({
+			method: URLMethods.GET,
+			path,
+			params: params || {},
+			headers: {
+				Authorization: `Bearer `,
+			},
+		});
+	};
+
+	const post = async ({ path, params }: URLInterface) => {
+		return getResponse({
+			method: URLMethods.POST,
+			path,
+			params: params || {},
+			headers: {},
+		});
+	};
+
+	const postAuth = async ({ path, params }: URLInterface) => {
+		return getResponse({
+			method: URLMethods.POST,
+			path,
+			params: params || {},
+			headers: {
+				Authorization: `Bearer `,
+			},
+		});
+	};
+
+	return {
+		...toRefs(state),
+		get,
+		getAuth,
+		post,
+		postAuth,
+	};
+}
--- src/store/user.ts
+++ src/store/user.ts
@@ -1,4 +1,5 @@
 import { createStore } from 'pinia';
+import useHttp, { HttpResponse } from '@/hooks/useHttp';
 
 type UserState = {
 	login: any;
@@ -10,12 +11,30 @@ export const useUserStore = createStore({
 	id: 'user',
 	state: (): UserState => ({
 		login: '',
-		user: 'Test',
+		user: '',
 		vehicles: [],
 	}),
 	actions: {
-		change() {
-			this.user = 'Bob';
+		resetUser() {
+			this.login = '';
+			this.user = '';
+			this.vehicles = [];
+		},
+
+		// async isLoggedIn() {},
+
+		async logUserIn(params: { username: string; password: string }) {
+			const { post } = useHttp();
+			const response: HttpResponse = await post({
+				path: 'auth/login',
+				params,
+			});
+
+			if (response.success) {
+				// ...
+			}
+
+			return response;
 		},
 	},
 });
