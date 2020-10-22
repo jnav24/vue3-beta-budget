28494168e63628fa28e12bb2b53b70d7caf40054
Justin Navarro <info@justinnavarro.net>
2020-10-22 08:36:23 -0400

    setup the new form elements

--- src/components/ui-elements/form/Checkbox.vue
+++ src/components/ui-elements/form/Checkbox.vue
@@ -1,7 +1,70 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref, inject, computed, onMounted } from 'vue';
+import Button from '@/components/ui-elements/form/Button.vue';
+import CheckIcon from '@/components/ui-elements/icons/CheckIcon.vue';
+import { FormProvider } from '@/components/ui-elements/form/Form';
+import Label from '@/components/ui-elements/form/Label.vue';
 
-export default defineComponent({});
+export default defineComponent({
+	components: {
+		Button,
+		CheckIcon,
+		Label,
+	},
+	props: {
+		label: {
+			required: true,
+			type: String,
+		},
+		rules: {
+			default: () => ({}),
+			type: Object,
+		},
+		value: {
+			required: true,
+			type: String,
+		},
+	},
+	setup(props, { emit }) {
+		const labelId = ref<string>('');
+		const FormContext = inject<any>(FormProvider, undefined);
+
+		onMounted(() => {
+			if (props.label && !!FormContext) {
+				labelId.value = FormContext.setupForm(props.label, props.rules);
+				FormContext.validateField(labelId.value, props.value, true);
+			}
+		});
+
+		const error = computed(() => {
+			if (FormContext && FormContext.formElements[labelId.value]) {
+				return FormContext.formElements[labelId.value].error;
+			}
+
+			return null;
+		});
+
+		const updateValue = (inputValue: string) => {
+			if (FormContext) {
+				FormContext.validateField(labelId.value, inputValue);
+			}
+			emit('update:value', inputValue);
+		};
+
+		return {
+			error,
+			labelId,
+			updateValue,
+		};
+	},
+});
 </script>
 
-<template></template>
+<template>
+	<div class="flex flex-row items-center">
+		<Button class="w-4 h-4" @on-click="updateValue()">
+			<CheckIcon class="w-4 h-4" />
+		</Button>
+		<Label :error="error" :labelId="labelId" :label="label" />
+	</div>
+</template>
--- src/components/ui-elements/form/Textarea.vue
+++ src/components/ui-elements/form/Textarea.vue
@@ -1,7 +1,75 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref, inject, computed, onMounted } from 'vue';
+import { FormProvider } from '@/components/ui-elements/form/Form';
+import Label from '@/components/ui-elements/form/Label.vue';
 
-export default defineComponent({});
+export default defineComponent({
+	components: {
+		Label,
+	},
+	props: {
+		label: {
+			required: true,
+			type: String,
+		},
+		rules: {
+			default: () => ({}),
+			type: Object,
+		},
+		value: {
+			required: true,
+			type: String,
+		},
+	},
+	setup(props, { emit }) {
+		const labelId = ref<string>('');
+		const FormContext = inject<any>(FormProvider, undefined);
+
+		onMounted(() => {
+			if (props.label && !!FormContext) {
+				labelId.value = FormContext.setupForm(props.label, props.rules);
+				FormContext.validateField(labelId.value, props.value, true);
+			}
+		});
+
+		const error = computed(() => {
+			if (FormContext && FormContext.formElements[labelId.value]) {
+				return FormContext.formElements[labelId.value].error;
+			}
+
+			return null;
+		});
+
+		const updateValue = (inputValue: string) => {
+			if (FormContext) {
+				FormContext.validateField(labelId.value, inputValue);
+			}
+			emit('update:value', inputValue);
+		};
+
+		return {
+			error,
+			labelId,
+			updateValue,
+		};
+	},
+});
 </script>
 
-<template></template>
+<template>
+	<div>
+		<Label :error="error" :labelId="labelId" :label="label" />
+		<textarea
+			:id="labelId"
+			class="w-full p-2 mt-2 border rounded outline-none"
+			:class="{
+				'border-gray-300 focus:border-primary': !error,
+				'border-red-600': error,
+			}"
+			:value="value"
+			@blur="updateValue($event.target.value)"
+			@input="updateValue($event.target.value)"
+			:aria-labelledby="labelId"
+		/>
+	</div>
+</template>
