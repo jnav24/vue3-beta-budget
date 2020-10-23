d1ab9922eccd1ed27d38e70f7de2fa3629ea9c95
Justin Navarro <info@justinnavarro.net>
2020-10-23 18:02:04 -0400

    update expense provider

--- src/components/modals/ExpenseFormProvider.ts
+++ src/components/modals/ExpenseFormProvider.ts
@@ -1,25 +1,34 @@
-import { defineComponent, provide, ref } from 'vue';
+import { computed, defineComponent, provide } from 'vue';
 import { BudgetExpense } from '@/store/budget';
+import { ComputedRef } from '@vue/reactivity';
 
 export const ExpenseFormContext = Symbol('Expense Form Provider');
 
 export type ExpenseFormContextType = {
-	data: BudgetExpense,
-	closeModal: (data: Record<string, string>) => void,
-	currentType: any,
-	editMode: any,
-	extractFormValues: (form: any) => Record<string, string>,
+	data: BudgetExpense;
+	closeModal: (data: Record<string, string>) => void;
+	currentType: ComputedRef<string>;
+	editMode: ComputedRef<boolean>;
+	extractFormValues: (form: any) => Record<string, string>;
 };
 
 export default defineComponent({
 	props: {
+		editMode: {
+			required: true,
+			type: Boolean,
+		},
 		data: {
 			default: () => ({}),
 			type: Object as () => BudgetExpense,
 		},
+		type: {
+			required: true,
+			type: String,
+		},
 	},
 	setup(props, { emit }) {
-		const currentType = ref('');
+		const currentType = computed(() => props.type);
 		const closeModal = (data = {}) => {
 			if (Object.keys(data).length) {
 				// save data and update the state
@@ -27,11 +36,13 @@ export default defineComponent({
 
 			emit('close-modal');
 		};
-		const editMode = ref(false);
+		const editMode = computed(() => props.editMode);
 
 		const extractFormValues = (form: any) => {
 			const result: Record<string, string> = {};
-			Object.keys(form).forEach(value => result[value] = form[value].value);
+			Object.keys(form).forEach(
+				value => (result[value] = form[value].value)
+			);
 			return result;
 		};
 
