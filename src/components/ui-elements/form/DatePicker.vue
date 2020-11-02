000e0daa9c5a1c98289d621c5b93ff93f34adf03
Justin Navarro <info@justinnavarro.net>
2020-11-02 10:44:26 -0500

    start formatting the picker

--- src/components/ui-elements/form/DatePicker.vue
+++ src/components/ui-elements/form/DatePicker.vue
@@ -3,10 +3,14 @@ import { computed, defineComponent, inject, onMounted, ref } from 'vue';
 import { FormProvider } from '@/components/ui-elements/form/Form';
 import Label from '@/components/ui-elements/form/Label.vue';
 import CalendarIcon from '@/components/ui-elements/icons/CalendarIcon.vue';
+import ChevronLeftIcon from '@/components/ui-elements/icons/ChevronLeftIcon.vue';
+import ChevronRightIcon from '@/components/ui-elements/icons/ChevronRightIcon.vue';
 
 export default defineComponent({
 	components: {
 		CalendarIcon,
+		ChevronLeftIcon,
+		ChevronRightIcon,
 		Label,
 	},
 	props: {
@@ -24,9 +28,10 @@ export default defineComponent({
 		},
 	},
 	setup(props, { emit }) {
+		const days = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
 		const labelId = ref<string>('');
 		const FormContext = inject<any>(FormProvider, undefined);
-		const selected = ref(false);
+		const selected = ref(true);
 
 		onMounted(() => {
 			if (props.label && !!FormContext) {
@@ -51,6 +56,7 @@ export default defineComponent({
 		};
 
 		return {
+			days,
 			error,
 			labelId,
 			selected,
@@ -92,12 +98,32 @@ export default defineComponent({
 			/>
 
 			<div
-				class="bg-white absolute left-0 bottom-0 w-64 h-64 shadow-2xl z-50 origin-top-left transform translate-y-64 transition ease-out duration-200"
+				class="bg-white p-4 absolute left-0 bottom-0 w-64 h-64 shadow-2xl z-50 origin-top-left transform translate-y-64 transition ease-out duration-200"
 				:class="{
 					'scale-100 opacity-100': selected,
 					'scale-0 opacity-0': !selected,
 				}"
-			></div>
+			>
+				<div
+					class="flex flex-row items-center justify-between text-gray-700"
+				>
+					<ChevronLeftIcon class="w-4 h-4" />
+					<span class="text-sm">October 2020</span>
+					<ChevronRightIcon class="w-4 h-4" />
+				</div>
+
+				<div class="grid-cols-7 gap-1 grid">
+					<span
+						class="text-center text-sm text-gray-500"
+						v-for="(day, index) in days"
+						:key="index"
+					>
+						{{ day }}
+					</span>
+				</div>
+
+				<div class="grid-cols-7 gap-1 grid"></div>
+			</div>
 		</div>
 		<span v-if="error" class="text-sm text-red-600">{{ error }}</span>
 	</div>
