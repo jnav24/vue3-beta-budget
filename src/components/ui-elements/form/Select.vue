4981dccfbff4c66e23c46ce836fd0d5371732c6b
Justin Navarro <info@justinnavarro.net>
2020-11-06 18:02:04 -0500

    validate select field on blur

--- src/components/ui-elements/form/Select.vue
+++ src/components/ui-elements/form/Select.vue
@@ -45,7 +45,7 @@ export default defineComponent({
 		const error = ref(false);
 		const selected = ref(false);
 		const dropDownItems = ref(null);
-		const labelId = ref(null);
+		const labelId = ref<string>('');
 		const FormContext = inject<any>(FormProvider, undefined);
 
 		onMounted(() => {
@@ -81,6 +81,7 @@ export default defineComponent({
 
 		const blurEvent = () => {
 			selected.value = false;
+			setValue(props.value);
 		};
 
 		watch(selected, n => {
