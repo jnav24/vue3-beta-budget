59b236cfc4bc510939d0caad4a022af7ca6f0ea3
Justin Navarro <info@justinnavarro.net>
2020-11-09 10:03:39 -0500

    setup expense modal and slideover on budget template

--- src/views/dashboard/BudgetTemplate.vue
+++ src/views/dashboard/BudgetTemplate.vue
@@ -1,24 +1,83 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref, watch, watchEffect } from 'vue';
 import BudgetTemplateTable from '@/components/tables/BudgetTemplateTable.vue';
 import Button from '@/components/ui-elements/form/Button.vue';
+import ExpenseModal from '@/components/modals/ExpenseModal.vue';
+import ExpenseSlideover from '@/components/slideovers/ExpenseSlideover.vue';
 import SaveIcon from '@/components/ui-elements/icons/SaveIcon.vue';
 import { useTemplateStore } from '@/store';
+import { BudgetExpense } from '@/store/budget';
 
 export default defineComponent({
 	components: {
 		BudgetTemplateTable,
 		Button,
+		ExpenseModal,
+		ExpenseSlideover,
 		SaveIcon,
 	},
 	setup() {
 		const templateStore = useTemplateStore();
-		return { expenses: templateStore.expenses };
+
+		const expenses = ref({});
+		const expenseData = ref({});
+		const selectedCategory = ref('');
+		const showModal = ref(false);
+
+		watch(showModal, n => {
+			if (!n) {
+				expenseData.value = {};
+			}
+		});
+
+		watchEffect(() => {
+			expenses.value = JSON.parse(JSON.stringify(templateStore.expenses));
+		});
+
+		const openExpense = (data: {
+			category: string;
+			expense?: BudgetExpense;
+		}) => {
+			if (data.expense) {
+				expenseData.value = data.expense;
+			}
+			selectedCategory.value = data.category;
+			showModal.value = true;
+		};
+
+		const updateLocalExpense = (data: BudgetExpense) => {
+			console.log(data);
+		};
+
+		return {
+			expenses,
+			expenseData,
+			openExpense,
+			selectedCategory,
+			showModal,
+			updateLocalExpense,
+		};
 	},
 });
 </script>
 
 <template>
+	<ExpenseModal
+		class="hidden lg:block"
+		v-model:show="showModal"
+		:data="expenseData"
+		:type="selectedCategory"
+		@update-budget="updateLocalExpense($event)"
+	/>
+
+	<ExpenseSlideover
+		class="block lg:hidden"
+		v-model:show="showModal"
+		:data="expenseData"
+		:type="selectedCategory"
+		@update-budget="updateLocalExpense($event)"
+	/>
+
 	<div class="w-full bg-gray-700">
 		<div
 			class="container mx-auto flex flex-col sm:flex-row justify-between items-center text-3xl border-b border-gray-400 mb-20 py-12 px-4 sm:px-0"
@@ -58,6 +117,7 @@ export default defineComponent({
 			:key="key"
 			:category="key"
 			:data="item"
+			@add-expense="openExpense($event)"
 		/>
 	</div>
 </template>
