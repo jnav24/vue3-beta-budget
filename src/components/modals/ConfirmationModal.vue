dc3a4e8aac4cff3e985270e6ed45f2146b201ec0
Justin Navarro <info@justinnavarro.net>
2020-11-20 18:05:50 -0500

    created new modals

--- src/components/modals/ConfirmationModal.vue
+++ src/components/modals/ConfirmationModal.vue
@@ -1,8 +1,8 @@
 <script lang="ts">
-import {computed, defineComponent, ref} from 'vue';
+import { computed, defineComponent, ref } from 'vue';
 import Modal from '@/components/modals/Modal.vue';
 import WarningIcon from '@/components/ui-elements/icons/WarningIcon.vue';
-import {BudgetList} from "@/store/budget";
+import { BudgetList } from '@/store/budget';
 
 export default defineComponent({
 	components: {
@@ -18,7 +18,11 @@ export default defineComponent({
 	setup(props, { emit }) {
 		const setClose = ref(false);
 
-		const setCloseModal = () => {
+		const setCloseModal = (e: boolean) => {
+			if (e) {
+				emit('confirm');
+			}
+
 			setClose.value = true;
 			setTimeout(() => (setClose.value = false), 1000);
 		};
@@ -34,7 +38,7 @@ export default defineComponent({
 
 <template>
 	<Modal :show="show" :set-close="setClose" @close="closeModal($event)">
-		<div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+		<div class="px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
 			<div class="sm:flex sm:items-start">
 				<div
 					class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10"
@@ -47,26 +51,25 @@ export default defineComponent({
 						class="text-lg leading-6 font-medium text-gray-900"
 						id="modal-headline"
 					>
-						Deactivate account
+						Remove Expense
 					</h3>
 					<div class="mt-2">
 						<p class="text-sm leading-5 text-gray-500">
-							Are you sure you want to deactivate your account?
-							All of your data will be permanently removed. This
-							action cannot be undone.
+							Are you sure you want to remove this expense?
 						</p>
 					</div>
 				</div>
 			</div>
 		</div>
 
-		<div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+		<div class="bg-gray-100 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse rounded-b-lg">
 			<span class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
 				<button
 					type="button"
 					class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-red-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-red transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+					@click="setCloseModal(true)"
 				>
-					Deactivate
+					Remove
 				</button>
 			</span>
 			<span
@@ -75,6 +78,7 @@ export default defineComponent({
 				<button
 					type="button"
 					class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+					@click="setCloseModal(false)"
 				>
 					Cancel
 				</button>
