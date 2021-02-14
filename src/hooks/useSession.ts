efafa19689966e5950301e1a03b70b51f95d9a2d
Justin Navarro <info@justinnavarro.net>
2021-02-14 00:14:42 -0500

    comment reset cookies

--- src/hooks/useSession.ts
+++ src/hooks/useSession.ts
@@ -27,6 +27,7 @@ export default function useSession() {
 		document.cookie = `${name} =; ${expires}; path=/; Max-Age=-99999999;`;
 	};
 
+	// @todo why did I use this?
 	const resetCookies = () => {
 		const newCookies = {};
 		const cookieList: string[] = document.cookie
@@ -47,7 +48,7 @@ export default function useSession() {
 	};
 
 	const getCookie = (name: string): string | null => {
-		resetCookies();
+		// resetCookies();
 		const match = document.cookie.match(new RegExp(name + '=([^;]+)'));
 		if (match) {
 			return match[1];
