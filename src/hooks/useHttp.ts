7d9042bcc99b706d6f09c7bd3f6c4419b4c3f92b
Justin N <janavarro24@gmail.com>
2021-03-21 08:30:14 -0400

    Update useHttp.ts
    fucking cors

--- src/hooks/useHttp.ts
+++ src/hooks/useHttp.ts
@@ -61,8 +61,8 @@ export default function useHttp() {
 				headers: { 
 					...headers, 
 					'Content-Type': 'application/json',
-          			'Access-Control-Allow-Origin': '*',
-          			'Access-Control-Allow-Headers': '*',
+          			//'Access-Control-Allow-Origin': '*',
+          			//'Access-Control-Allow-Headers': '*',
           			'Accept': 'application/json, text/plain'
 				},
 				...responseData,
