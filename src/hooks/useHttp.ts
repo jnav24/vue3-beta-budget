872ee79dfc572108a667a24aa7702f2e5803b6d1
Justin N <janavarro24@gmail.com>
2021-03-21 00:00:57 -0400

    Update useHttp.ts
    actual fix

--- src/hooks/useHttp.ts
+++ src/hooks/useHttp.ts
@@ -58,7 +58,7 @@ export default function useHttp() {
 					/^\/|\/$/g,
 					''
 				)}`,
-				{ ...headers, 'Access-Control-Allow-Origin': '*' },
+				headers: { ...headers, 'Access-Control-Allow-Origin': '*' },
 				...responseData,
 				withCredentials: true,
 			});
