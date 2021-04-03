bae1c23afd2928738a3af9d081c89f765dec61f1
Justin Navarro <info@justinnavarro.net>
2021-04-03 18:06:00 -0400

    setting up the update password form

--- src/components/partials/SettingsUpdatePassword.vue
+++ src/components/partials/SettingsUpdatePassword.vue
@@ -1,10 +1,35 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, reactive, ref } from 'vue';
+import Button from '@/components/ui-elements/form/Button.vue';
+import { Form } from '@/components/ui-elements';
+import Input from '@/components/ui-elements/form/Input.vue';
 import SettingsLayout from '@/components/layouts/SettingsLayout.vue';
 
 export default defineComponent({
 	components: {
+		Button,
+		Form,
+		Input,
 		SettingsLayout,
+	},
+	setup() {
+		const form = reactive({
+			current_password: {
+				rules: [],
+				value: '',
+			},
+			confirm_password: {
+				rules: [],
+				value: '',
+			},
+			new_password: {
+				rules: [],
+				value: '',
+			},
+		});
+		const valid = ref(false);
+
+		return { form, valid };
 	},
 });
 </script>
@@ -13,5 +38,34 @@ export default defineComponent({
 	<SettingsLayout
 		title="Update Password"
 		description="Ensure your account is using a long, random password to stay secure."
-	></SettingsLayout>
+	>
+		<Form v-model:valid="valid">
+			<Input
+				label="Current Password"
+				type="password"
+				v-model:value="form.current_password.value"
+				:rules="form.current_password.rules"
+			/>
+
+			<Input
+				label="New Password"
+				type="password"
+				v-model:value="form.new_password.value"
+				:rules="form.new_password.rules"
+			/>
+
+			<Input
+				label="Confirm Password"
+				type="password"
+				v-model:value="form.confirm_password.value"
+				:rules="form.confirm_password.rules"
+			/>
+
+			<Button
+				color="secondary"
+			>
+				Save
+			</Button>
+		</Form>
+	</SettingsLayout>
 </template>
