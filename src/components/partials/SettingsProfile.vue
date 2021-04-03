04cccf02e814b0cf8730d892dacc14c15363c2d3
Justin Navarro <info@justinnavarro.net>
2021-04-03 17:56:03 -0400

    setup settings components

---
+++ src/components/partials/SettingsProfile.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import SettingsLayout from '@/components/layouts/SettingsLayout.vue';
+
+export default defineComponent({
+	components: {
+		SettingsLayout,
+	},
+});
+</script>
+
+<template>
+	<section class="pt-12">
+		<SettingsLayout
+			title="Profile Information"
+			description="Update your account's profile information and email address."
+		></SettingsLayout>
+	</section>
+</template>
---
+++ src/components/partials/SettingsUpdatePassword.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import SettingsLayout from '@/components/layouts/SettingsLayout.vue';
+
+export default defineComponent({
+	components: {
+		SettingsLayout,
+	},
+});
+</script>
+
+<template>
+	<SettingsLayout
+		title="Update Password"
+		description="Ensure your account is using a long, random password to stay secure."
+	></SettingsLayout>
+</template>
