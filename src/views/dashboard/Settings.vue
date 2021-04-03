df4e08dace91d03c3a3aaa7a24b53c0653580155
Justin Navarro <info@justinnavarro.net>
2021-04-03 17:47:26 -0400

    created settings page

---
+++ src/views/dashboard/Settings.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import SettingsProfile from '@/components/partials/SettingsProfile.vue';
+import SettingsUpdatePassword from '@/components/partials/SettingsUpdatePassword.vue';
+
+export default defineComponent({
+	components: {
+		SettingsProfile,
+		SettingsUpdatePassword,
+	},
+});
+</script>
+
+<template>
+	<section class="container mx-auto">
+		<SettingsProfile />
+		<SettingsUpdatePassword />
+	</section>
+</template>
