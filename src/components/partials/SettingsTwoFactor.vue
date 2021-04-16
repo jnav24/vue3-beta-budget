b38ecd94b31e469bbf1a9277b31daaa7a67a0ffc
Justin Navarro <info@justinnavarro.net>
2021-04-16 17:40:36 -0400

    add two factor component to the settings page

---
+++ src/components/partials/SettingsTwoFactor.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import SettingsLayout from '@/components/layouts/SettingsLayout.vue';
+
+export default defineComponent({
+	components: {
+		SettingsLayout,
+	},
+});
+</script>
+
+<template>
+	<section class="pt-12">
+		<SettingsLayout
+			title="Two Factor Authentication"
+			description="Add additional security to your account by using two factor authentication"
+		>
+			hello
+		</SettingsLayout>
+	</section>
+</template>
--- src/views/dashboard/Settings.vue
+++ src/views/dashboard/Settings.vue
@@ -1,11 +1,13 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import SettingsProfile from '@/components/partials/SettingsProfile.vue';
+import SettingsTwoFactor from '@/components/partials/SettingsTwoFactor.vue';
 import SettingsUpdatePassword from '@/components/partials/SettingsUpdatePassword.vue';
 
 export default defineComponent({
 	components: {
 		SettingsProfile,
+		SettingsTwoFactor,
 		SettingsUpdatePassword,
 	},
 });
@@ -15,5 +17,6 @@ export default defineComponent({
 	<section class="container mx-auto">
 		<SettingsProfile />
 		<SettingsUpdatePassword />
+		<SettingsTwoFactor />
 	</section>
 </template>
