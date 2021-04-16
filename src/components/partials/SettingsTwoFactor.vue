f9b9645b2d4e6612f5d4afd4bd87ff350fb8d597
Justin Navarro <info@justinnavarro.net>
2021-04-16 19:04:28 -0400

    add calls to 2fa endpoints

--- src/components/partials/SettingsTwoFactor.vue
+++ src/components/partials/SettingsTwoFactor.vue
@@ -1,10 +1,34 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import SettingsLayout from '@/components/layouts/SettingsLayout.vue';
+import useHttp from '@/hooks/useHttp';
 
 export default defineComponent({
 	components: {
 		SettingsLayout,
+	},
+	setup() {
+		const { postAuth, getAuth } = useHttp();
+
+		const enableTwoFactor = async () => {
+			const { state, data } = await postAuth({
+				path: '/user/two-factor-authentication',
+			});
+		};
+
+		const getQRCode = async () => {
+			const { state, data } = await getAuth({
+				path: '/user/two-factor-qr-code',
+			});
+		};
+
+		const getRecoveryCodes = async () => {
+			const { state, data } = await getAuth({
+				path: '/user/two-factor-recovery-codes',
+			});
+		};
+
+		return {};
 	},
 });
 </script>
