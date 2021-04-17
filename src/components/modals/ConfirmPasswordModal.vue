8dca79cda2495024ba6d87213e0651022529a5b1
Justin Navarro <info@justinnavarro.net>
2021-04-17 17:07:22 -0400

    show error message on failed login

--- src/components/modals/ConfirmPasswordModal.vue
+++ src/components/modals/ConfirmPasswordModal.vue
@@ -1,5 +1,6 @@
 <script lang="ts">
 import { defineComponent, ref, watch } from 'vue';
+import Alert from '@/components/ui-elements/Alert.vue';
 import Button from '@/components/ui-elements/form/Button.vue';
 import Form from '@/components/ui-elements/form/Form';
 import Input from '@/components/ui-elements/form/Input.vue';
@@ -8,6 +9,7 @@ import useHttp from '@/hooks/useHttp';
 
 export default defineComponent({
 	components: {
+		Alert,
 		Button,
 		Form,
 		Input,
@@ -21,6 +23,7 @@ export default defineComponent({
 	},
 	setup(props, { emit }) {
 		const { postAuth } = useHttp();
+		const errorMessage = ref('');
 		const passwordField = ref('');
 		const setClose = ref(false);
 		const showModal = ref(false);
@@ -29,26 +32,30 @@ export default defineComponent({
 		const setCloseModal = () => {
 			setClose.value = true;
 			setTimeout(() => {
+				errorMessage.value = '';
+				passwordField.value = '';
+				isValid.value = false;
 				setClose.value = false;
 				showModal.value = false;
 			}, 250);
 		};
 
 		const handleConfirmPassword = async () => {
-			const { error, success, data } = await postAuth({
+			const { error, success } = await postAuth({
 				path: '/user/confirm-password',
 				params: {
 					password: passwordField.value,
 				},
 			});
-			console.log(data);
-			console.log(error);
-			console.log(success);
+
 			if (success) {
 				emit('handle-confirm', true);
 				setCloseModal();
 			}
-			// if error, show the alert and keep modal open
+
+			if (error.length) {
+				errorMessage.value = error;
+			}
 		};
 
 		watch(
@@ -64,6 +71,7 @@ export default defineComponent({
 
 		return {
 			closeModal: (e: boolean) => emit('update:show', e),
+			errorMessage,
 			handleConfirmPassword,
 			passwordField,
 			setClose,
@@ -84,6 +92,11 @@ export default defineComponent({
 					In order to perform this action, you must confirm your
 					password.
 				</p>
+				<Alert
+					:message="errorMessage"
+					type="error"
+					v-if="errorMessage.length"
+				/>
 				<Form v-model:valid="isValid">
 					<Input
 						label="Password"
