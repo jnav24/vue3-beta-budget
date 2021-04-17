fea99f04f91546bb6744457d2510ea44e7d01edb
Justin Navarro <info@justinnavarro.net>
2021-04-17 18:34:24 -0400

    style the 2fa information

--- src/components/partials/SettingsTwoFactor.vue
+++ src/components/partials/SettingsTwoFactor.vue
@@ -110,8 +110,48 @@ export default defineComponent({
 				*You will need an authenticator app like Authy or Google
 				Authenticator to use two factor.
 			</p>
-			<div v-html="qrCode"></div>
-			<pre>{{ recoveryCodes }}</pre>
+			<div class="border-t border-gray-300 mt-4">
+				<p class="text-danger mt-4 text-sm">
+					Note: The information below will only be shown once.
+				</p>
+				<div class="flex flex-row mt-4 mb-8">
+					<div class="text-gray-700 text-sm mr-4 space-y-4">
+						<p>
+							Two factor authentication is now enabled. Scan the
+							following QR Code using your phone's authenticator
+							application.
+						</p>
+						<p>
+							While two factor authentication is enabled, you will
+							be prompted for a secure random token during
+							authentication. You may retrieve this token from
+							your phone's Google Authenticator app.
+						</p>
+					</div>
+					<div v-html="qrCode"></div>
+				</div>
+				<p class="text-gray-700 text-sm mb-2">
+					Store these recovery codes in a secure password manager.
+					They can be used to recover access to your account if your
+					two factor authentication device is lost.
+				</p>
+				<div
+					class="flex flex-row justify-between text-gray-700 bg-gray-200 border border-gray-300 py-2 pl-8 pr-4"
+				>
+					<ul v-for="(v, i) in [1, 2]" :key="i">
+						<li
+							class="list-disc mb-2 text-sm"
+							v-for="(code, index) in [...recoveryCodes].splice(
+								i * 4,
+								v * 4
+							)"
+							:key="index"
+						>
+							{{ code }}
+						</li>
+					</ul>
+				</div>
+			</div>
 		</SettingsLayout>
 	</section>
 </template>
