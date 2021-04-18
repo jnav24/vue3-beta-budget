54a5c9e05b4fc98e447c28b920ceed7438cba419
Justin Navarro <info@justinnavarro.net>
2021-04-17 23:09:16 -0400

    setup 2 factor form

--- src/components/partials/forms/TwoFactor.vue
+++ src/components/partials/forms/TwoFactor.vue
@@ -1,7 +1,29 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
+import Alert from '@/components/ui-elements/Alert.vue';
+import { Form } from '@/components/ui-elements';
+import Input from '@/components/ui-elements/form/Input.vue';
+import LoaderIcon from '@/components/ui-elements/icons/LoaderIcon.vue';
 
-export default defineComponent({});
+export default defineComponent({
+	components: {
+		Alert,
+		Form,
+		Input,
+		LoaderIcon,
+	},
+	setup() {
+		const totp = ref('');
+		const disableSubmit = ref(false);
+		const isValid = ref(false);
+
+		const handleClick = () => {
+			console.log('blah');
+		};
+
+		return { disableSubmit, handleClick, isValid, totp };
+	},
+});
 </script>
 
 <template>
@@ -11,5 +33,32 @@ export default defineComponent({});
 		>
 			2-Step Auth
 		</h1>
+
+		<Alert type="error" message="testing..." />
+
+		<Form v-model:valid="isValid">
+			<Input
+				label="Enter 6 digit code"
+				v-model:value="totp"
+				:rules="['required', 'numeric', 'max:6', 'min:6']"
+			/>
+
+			<button
+				@click="handleClick()"
+				class="w-full py-2 rounded flex flex-row items-center justify-center"
+				type="button"
+				:disabled="!valid || disableSubmit"
+				:class="{
+					'bg-secondary shadow-md': valid && !disableSubmit,
+					'bg-gray-300 text-gray-600': !valid || disableSubmit,
+				}"
+			>
+				<LoaderIcon
+					v-if="disableSubmit"
+					class="animate-spin -ml-1 mr-3 h-5 w-5 text-gray-600"
+				/>
+				<span v-if="!disableSubmit">Verify</span>
+			</button>
+		</Form>
 	</div>
 </template>
